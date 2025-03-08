214518fec0925c3f9f99edb47a44dd010ead932a
Champu <117635421+TheChampu@users.noreply.github.com>
2025-03-08 11:01:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,12 +16,12 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @FallenxBot on Telegram by /id
+# Get this value from @TheChampuBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6399386263))
 
 ## Fill these variables if you're deploying on heroku.
@@ -43,7 +43,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/akaChampu")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/TheChampuClub")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
